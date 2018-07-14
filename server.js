9778c2b58452fb6145285b811ba942f425bab3f3
Roy Prigat <rp2719@columbia.edu>
2018-07-14 20:18:53 +0300

    committing changes Sat Jul 14 20:18:53 IDT 2018

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+
+
+
+
+// for any request that doesn't match one above,
+// send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
