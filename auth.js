13e28aacd57a755959a0e812123ec572857a096c
Roy Prigat <rp2719@columbia.edu>
2018-07-15 21:45:38 +0300

    committing changes Sun Jul 15 21:45:38 IDT 2018

---
+++ auth.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const keys = JSON.parse(fs.readFileSync('client_secret.json'));
+const clientID = keys.web.client_id;
+const clientSecret = keys.web.client_secret;
+const redirectURI = keys.web.redirect_uris[0];
+
+const scopes = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/youtube.readonly'
+];
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+module.exports = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.use(new GoogleStrategy({
+            clientID: clientID,
+            clientSecret: clientSecret,
+            callbackURL: redirectURI
+        },
+        (token, refreshToken, profile, done) => {
+            return done(null, {
+                profile: profile,
+                token: token
+            });
+        }));
+};
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport'),
+const auth = require('./auth');
 
 const app = express();
 
+auth(passport);
+app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req, res) => {
+  res.json({
+      status: 'session cookie not set'
+  });
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/'}),
+    (req, res) => {}
+);
 
 // send back React's index.html file for all requests
 app.get('*', (req, res) => {
