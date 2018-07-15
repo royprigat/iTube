9d56820a89f4b12852d08e67c6c9f134d24c7a49
Roy Prigat <rp2719@columbia.edu>
2018-07-15 17:08:25 +0300

    committing changes Sun Jul 15 17:08:25 IDT 2018

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,44 @@
+import { withStyles } from "@material-ui/core/styles";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import * as React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      contrastText: "#ba68c8",
+      dark: "#babdbe",
+      light: "#ffffff",
+      main: "#eceff1"
+    },
+    secondary: {
+      contrastText: "#fafafa",
+      dark: "#883997",
+      light: "#ee98fb",
+      main: "#ba68c8"
+    }
+  }
+});
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div>
-        <Navbar />
-      </div>
-    );
+const styles = {
+  paper: {
+    padding: 10
+  },
+  root: {
+    flexGrow: 1
   }
 };
 
-export default App;
+function App(props: { classes: any; }) {
+  const { classes } = props;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <Navbar />
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default withStyles(styles)(App);
