8c9e37b061ecfe3cf2356c2f220fb6be462e5c3d
Roy Prigat <rp2719@columbia.edu>
2018-07-16 20:47:43 +0300

    deploy

---
+++ client/src/auth.ts
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyAPfO1SyOLFUwANAc3NxvqiTcGooNoTr68",
+    authDomain: "itube-210217.firebaseapp.com",
+    clientID: "804723299124-pee9ef54gldpgiqlch51qsn73cbmnfef.apps.googleusercontent.com",
+    databaseURL: "https://itube-210217.firebaseio.com",
+    messagingSenderId: "804723299124",
+    projectId: "itube-210217",
+    scopes: ["https://www.googleapis.com/auth/youtube.readonly"],
+    storageBucket: "itube-210217.appspot.com",
+};
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# Add all changes.
+git add -A
+git commit -m "deploy"
 git push heroku master
