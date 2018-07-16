ddaaaab58a91caffebbc0f771d1bb511df10f6a8
Roy Prigat <rp2719@columbia.edu>
2018-07-16 20:39:18 +0300

    committing changes Mon Jul 16 20:39:18 IDT 2018

--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -1,8 +1,14 @@
+import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import Paper from "@material-ui/core/Paper";
+import * as firebase from 'firebase';
 import * as React from "react";
+import googleIcon from "../../assets/google.svg";
+import { config } from "../../auth";
 import "./Login.css";
+
+firebase.initializeApp(config);
 
 interface ILoginProps {
   setToken?: any;
@@ -15,6 +21,7 @@ function Login(props: ILoginProps) {
       <Paper elevation={8} className="form-container">
         <FormControl component="fieldset" className="form">
           <h2>iTube</h2>
+          <Button className="signBtn"><img src={googleIcon} className="google-icon"/><span style={{marginLeft: '20px'}}>Sign in with Google</span></Button>
           <FormHelperText className="credit">
             Made with ♥️ by Roy Prigat
           </FormHelperText>
